fc63adc2f9abef64656d4e24648bd187afb09411
rgreen32 <ray.greenm@gmail.com>
2023-05-20 15:15:26 -0500

    server test code

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
 
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+
+app.get("/", function(request, response){
+    response.send("test")
+})
+
+app.listen(3001, () => {
+    console.log("Listening on port 3001")
+})
