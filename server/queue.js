5e0f630f3b52ea9ec2e676c6b2706f2942233904
rgreen32 <ray.greenm@gmail.com>
2023-05-21 15:29:45 -0500

    added error log to redis client

--- server/queue.js
+++ server/queue.js
@@ -5,6 +5,8 @@ dotenv.config()
 class QueueObject{
     constructor(){
         this.redisClient = redis.createClient({ url: process.env.REDIS_CONNECTION_STRING })
+        this.redisClient.on('error', err => console.log('Redis Client Error', err));
+
     }
     
     async getQueue(){
