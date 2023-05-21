d651008941b1f4d6481aeec3a3f457e81abe3b90
rgreen32 <ray.greenm@gmail.com>
2023-05-20 19:29:49 -0500

    tserver

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
-
 const express = require("express")
 const cors = require("cors")
-
 const app = express()
+const expressWs = require("express-ws")(app)
+
 app.use(cors())
 
-app.get("/", function(request, response){
-    response.send("test")
-})
+var queue = []
+
+app.use(function (request, response, next) {
+    return next();
+});
+
+
+app.ws('/', function (ws, request) {
+    ws.on('message', function (msg) {
+        queue.push(msg);
+    });
+    console.log('socket', request.testing);
+});
+
+app.get('/', function (request, response, next) {
+    response.send(queue)
+});
 
 app.listen(3001, () => {
     console.log("Listening on port 3001")
