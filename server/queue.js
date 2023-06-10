1b35e409c86fa2de23c138b6786d2bff011239c6
rgreen32 <ray.greenm@gmail.com>
2023-06-09 21:14:58 -0500

    removed connect statments from queue methods

--- server/queue.js
+++ server/queue.js
@@ -6,34 +6,27 @@ class QueueObject{
     constructor(){
         this.redisClient = redis.createClient({ url: process.env.REDIS_CONNECTION_STRING })
         this.redisClient.on('error', err => console.log('Redis Client Error', err));
+        this.redisClient.connect().then(()=>{ console.log("Successfully Connected...") })
     }
     
     async getQueue(){
-        await this.redisClient.connect()
-
         let queueString = await this.redisClient.get("queue")
         let queue = JSON.parse(queueString)
-
-        await this.redisClient.disconnect()
+        if(!queue){ queue = [] }
         return queue
     }
 
     async addStudentToQueue(student){
-        await this.redisClient.connect()
 
-        let queueString = await this.redisClient.get("queue")
-        let queue = JSON.parse(queueString)
+        let queue = await this.getQueue()
         queue.push(student)
         await this.redisClient.set('queue', JSON.stringify(queue))
 
-        await this.redisClient.disconnect()
     }
 
     async removeStudentFromQueue(studentName){
-        await this.redisClient.connect()
 
-        let queueString = await this.redisClient.get("queue")
-        let queue = JSON.parse(queueString)
+        let queue = await this.getQueue()
         let studentIndex = queue.findIndex((element) => {
             if(element.name === studentName){
                 return true
@@ -46,7 +39,6 @@ class QueueObject{
         }
         await this.redisClient.set("queue", JSON.stringify(queue))
         
-        await this.redisClient.disconnect()
     }
 }
 
