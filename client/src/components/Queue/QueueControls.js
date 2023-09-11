87f4eece679569149ffd57e7c54df4edab1729c4
Curtrick Walton <curtrickw@yahoo.com>
2023-09-11 16:50:06 +0000

    added description form and display only the first user in the queue

--- client/src/components/Queue/QueueControls.js
+++ client/src/components/Queue/QueueControls.js
@@ -1,63 +1,138 @@
-import { Button } from "react-bootstrap"
-import { useAuth0 } from "@auth0/auth0-react"
-import useQueueAPIClient from "../../clients/QueueAPI"
+import { Button, Form, Modal } from "react-bootstrap";
+import { useAuth0 } from "@auth0/auth0-react";
+import useQueueAPIClient from "../../clients/QueueAPI";
+import { useState } from "react";
 
-function QueueControls(props){
-    const user = props.user
-    const { loginWithRedirect, logout, isAuthenticated } = useAuth0()
-    const QueueAPIClient  = useQueueAPIClient()
+function QueueControls(props) {
+  const user = props.user;
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+  const QueueAPIClient = useQueueAPIClient();
+  const [description, setDescription] = useState("");
+  const [show, setShow] = useState(false);
+  const [isDescriptionValid, setIsDescriptionValid] = useState(true); // Added state for validation
 
-    async function joinQueue(){
-        let userId = user.sub.split("|")[1]
-        let studentObject = {email: user.email, name: user.given_name, description: ""}
-        let oldQueue = [...props.queue]
+  const handleShow = () => {
+    setShow(true);
+  };
+  const handleClose = () => {
+    setShow(false);
+    setIsDescriptionValid(true); // Reset validation when the modal is closed
+  }
 
-        QueueAPIClient.post("/", studentObject).catch((error)=>{
-            console.log(error.message)
-            props.setQueue(oldQueue)
-            props.setStudentIsInQueue(false)
-        })
-
-        let newQueue = [...props.queue, studentObject]
-        props.setQueue(newQueue)
-        props.setStudentIsInQueue(true)
-
+  async function joinQueue(e) {
+    e.preventDefault();
+    if (description.trim() === "") {
+      setIsDescriptionValid(false); // Set validation to false if the description is empty
+      return; // Do not proceed if the description is empty
     }
+    setShow(false);
+    let userId = user.sub.split("|")[1];
+    let studentObject = {
+      email: user.email,
+      name: user.given_name,
+      description: description,
+    };
+    let oldQueue = [...props.queue];
 
+    QueueAPIClient.post("/", studentObject).catch((error) => {
+      console.log(error.message);
+      props.setQueue(oldQueue);
+      props.setStudentIsInQueue(false);
+    });
 
-    async function leaveQueue(){
-        let userEmail = user.email
-        let oldQueue = [...props.queue]
+    let newQueue = [...props.queue, studentObject];
+    props.setQueue(newQueue);
+    props.setStudentIsInQueue(true);
+    console.log(user.name, user.description);
+  }
 
-        QueueAPIClient.delete("/", {params: {email: userEmail}}).catch((error) =>{
-            console.log(error.message)
-            props.setQueue(oldQueue)
-            props.setStudentIsInQueue(true)
-        })
-        let newQueue = props.queue.filter((student)=>{
-            if(student.email !== userEmail){
-                return true
-            }else{
-                return false
-            }
-        })
-        props.setQueue(newQueue)
-        props.setStudentIsInQueue(false)
-    }
+  async function leaveQueue() {
+    let userEmail = user.email;
+    let oldQueue = [...props.queue];
 
+    QueueAPIClient.delete("/", { params: { email: userEmail } }).catch(
+      (error) => {
+        console.log(error.message);
+        props.setQueue(oldQueue);
+        props.setStudentIsInQueue(true);
+      }
+    );
+    let newQueue = props.queue.filter((student) => {
+      if (student.email !== userEmail) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    props.setQueue(newQueue);
+    props.setStudentIsInQueue(false);
+  }
 
-    return <div className="QueueControls">
-                { isAuthenticated && (<>
-                                        <img className="ProfilePicture" src={user?.picture} />
-                                        <Button onClick={!props.studentIsInQueue ? joinQueue : leaveQueue} className="EnqueueButton">
-                                            {!props.studentIsInQueue ? "Join Queue" : "Leave Queue"}
-                                        </Button>
-                                      </>)
-                }
-                <Button onClick={ isAuthenticated !== true ? loginWithRedirect: logout } className="AuthButton">
-                    { isAuthenticated !== true ? "Login": "Logout" }
+  return (
+    <div className="QueueControls">
+      {isAuthenticated && (
+        <>
+          <img className="ProfilePicture" src={user?.picture} />
+
+          {!props.studentIsInQueue ? (
+            <Button variant="primary" onClick={handleShow}>
+              Submit Request
+            </Button>
+          ) : (
+            <Button variant="danger" onClick={leaveQueue}>
+              Leave Queue
+            </Button>
+          )}
+
+          <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>State Your Problem</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="description">
+                  <Form.Label>State Your Problem</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="What is your problem?"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    isInvalid={!isDescriptionValid} // Apply validation state
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Description is required.
+                  </Form.Control.Feedback>
+                </Form.Group>
+
+                <Button
+                  onClick={!props.studentIsInQueue ? joinQueue : leaveQueue}
+                  className="EnqueueButton"
+                >
+                  {!props.studentIsInQueue ? "Join Queue" : "Leave Queue"}
                 </Button>
-           </div>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
+      )}
+      <Button
+        onClick={isAuthenticated !== true ? loginWithRedirect : logout}
+        className="AuthButton"
+      >
+        {isAuthenticated !== true ? "Login" : "Logout"}
+      </Button>
+    </div>
+  );
 }
 
-export default QueueControls
+export default QueueControls;
--- client/src/components/Queue/index.js
+++ client/src/components/Queue/index.js
@@ -1,71 +1,87 @@
-import { useEffect, useState } from "react"
-import StudentInQueue from "./StudentInQueue"
-import axios from "axios"
-import QueueControls from "./QueueControls"
-import './Queue.css';
+import React, { useEffect, useState } from "react";
+import StudentInQueue from "./StudentInQueue";
+import axios from "axios";
+import QueueControls from "./QueueControls";
+import "./Queue.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function Queue() {
-    const {user, isLoading} = useAuth0()
-    const [queue, setQueue] = useState([])
-    const [student, setStudent] = useState(undefined)
-    const [studentIsInQueue, setStudentIsInQueue] = useState(false)
-    async function updateQueue() {
-        let response = await axios.get(process.env.REACT_APP_API_URL)
-        let updatedQueue = response.data
-        setQueue(updatedQueue)
+  const { user, isLoading } = useAuth0();
+  const [queue, setQueue] = useState([]);
+  const [student, setStudent] = useState(undefined);
+  const [studentIsInQueue, setStudentIsInQueue] = useState(false);
+
+  async function updateQueue() {
+    let response = await axios.get(process.env.REACT_APP_API_URL);
+    let updatedQueue = response.data;
+    setQueue(updatedQueue);
+  }
+
+  async function removeStudentFromQueue(studentName) {
+    axios.delete(process.env.REACT_APP_API_URL + `?name=${studentName}`);
+    let studentIndex = queue.findIndex((element) => {
+      if (element.name === studentName) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    if (studentIndex !== -1) {
+      queue.splice(studentIndex, 1);
     }
+    let updatedQueue = [...queue];
+    setQueue(updatedQueue);
+  }
 
-    async function removeStudentFromQueue(studentName) {
-        axios.delete(process.env.REACT_APP_API_URL + `?name=${studentName}`)
-        let studentIndex = queue.findIndex((element) => {
-            if (element.name === studentName) {
-                return true
-            } else {
-                return false
-            }
-        })
-        if (studentIndex !== -1) {
-            queue.splice(studentIndex, 1)
+  useEffect(() => {
+    updateQueue();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      let userEmail = user.email;
+      queue.some((element) => {
+        console.log("element", element);
+        if (element.email === userEmail) {
+          setStudentIsInQueue(true);
         }
-        let updatedQueue = [...queue]
-        setQueue(updatedQueue)
+      });
     }
+  }, [queue, user]);
 
-    let queueHTML = []
+  // Render only the first user in the queue
+  let firstUserInQueue = null;
+  if (queue.length > 0) {
+    
+    firstUserInQueue = (
+      <StudentInQueue
+        removeStudentFromQueue={removeStudentFromQueue}
+        name={queue[0].name}
+        description={queue[0].description}
+      />
+    );
+  }
 
-    queueHTML = queue.map((element) => {
-        return <StudentInQueue removeStudentFromQueue={removeStudentFromQueue} name={element.name} />
-
-    })
-    useEffect(() => {
-        updateQueue()
-
-    }, [])
-
-    useEffect(()=>{
-        if(user){
-            let userEmail = user.email
-            queue.some((element)=>{
-                console.log("element", element)
-                if(element.email === userEmail){
-                    setStudentIsInQueue(true)
-                }
-            })
-        }
-    }, [queue, user])
-
-    return <>
-                <div className="Queue">
-
-                    <h1 style={{ fontSize: 60, margin: 0 }}> {queue?.length} </h1>
-
-                    <div className="students" style={{ marginTop: "5vh" }}>
-                        {queueHTML.length !== 0 ? queueHTML : <h2>Queue is empty</h2>}
-                    </div>
-
-                </div>
-                <QueueControls user={user} studentIsInQueue={studentIsInQueue} setStudentIsInQueue={setStudentIsInQueue} queue={queue} setQueue={setQueue} />
-            </>
+  return (
+    <div className="Queue">
+      <h1 style={{ fontSize: 60, margin: 0 }}> {queue?.length} </h1>
+      <div className="students" style={{ marginTop: "5vh" }}>
+        {firstUserInQueue || <h2>Queue is empty</h2>}
+      </div>
+      <QueueControls
+        user={user}
+        studentIsInQueue={studentIsInQueue}
+        setStudentIsInQueue={setStudentIsInQueue}
+        queue={queue}
+        setQueue={setQueue}
+      />
+      {user && user.description && (
+        <div className="UserDescription">
+          <p>{user.description}</p>
+        </div>
+      )}
+    </div>
+  );
 }
-export default Queue
+
+export default Queue;
